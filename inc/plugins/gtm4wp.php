5266307cc16465d38de1538fb1585ff91bf094a5
Nico Mollet <nico.mollet@gmail.com>
2017-04-12 14:53:26 +0200

    GTM new event

--- inc/plugins/gtm4wp.php
+++ inc/plugins/gtm4wp.php
@@ -10,6 +10,7 @@ function godspeed_gtm4wp_fix() {
         add_filter('gtm4wp_event-social', '__return_true');
         add_filter('gtm4wp_event-email-clicks', '__return_true');
         add_filter('gtm4wp_event-downloads', '__return_true');
+	    add_filter('gtm4wp_scroller-enabled', '__return_true');
     }
 }
 add_action('init', 'godspeed_gtm4wp_fix');
