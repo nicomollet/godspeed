92c1bd733cc0f4ec08bababeab6bd675e6ffa547
Nico Mollet <nico.mollet@gmail.com>
2017-06-19 17:47:53 +0200

    WooCommerce compatibility: removed all CSS

--- inc/front/support.php
+++ inc/front/support.php
@@ -7,8 +7,5 @@ function godspeed_support() {
     add_theme_support( 'post-thumbnails' );
     add_theme_support( 'title-tag' );
 
-    if ( in_array( 'woocommerce/woocommerce.php', apply_filters( 'active_plugins', get_option( 'active_plugins' ) ) ) ){
-        add_theme_support( 'woocommerce' );
-    }
 }
 add_action( 'after_setup_theme', 'godspeed_support' );
