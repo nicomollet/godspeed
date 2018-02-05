2837b13d656f0b52fe118dba86af0862a9f262cc
Nico Mollet <nico.mollet@gmail.com>
2018-02-05 11:22:37 +0100

    Move remove action for woothemes_updater_notice to customadmin

--- inc/plugins/woocommerce.php
+++ inc/plugins/woocommerce.php
@@ -37,9 +37,4 @@ function godspeed_remove_footer_credit () {
 }
 add_action( 'init', 'godspeed_remove_footer_credit', 10 );
 
-/**
- * Remove WooCommerce Updater
- */
-remove_action('admin_notices', 'woothemes_updater_notice');
-
 
